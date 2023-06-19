51d881a0c56a83406e2ceda05fc319d0717cdc41
John Boyle <boylejm@gmail.com>
2023-06-19 18:25:40 +0100

    generalize handling certs

--- run_server.sh
+++ run_server.sh
@@ -1,6 +1,7 @@
+CERTFILE=$1
+KEYFILE=$2
 source myenv/bin/activate
 export FLASK_APP=app.py
-#python3 -m flask run --host=0.0.0.0
 gunicorn --certfile=$CERTFILE --keyfile=$KEYFILE --bind 0.0.0.0:5000 app:app
 
 
